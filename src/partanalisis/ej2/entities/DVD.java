e749b2bcec589e55bd1581720d37c55e3a04dc81
Usuario <Usuario@DESKTOP-294PFD1>
2024-06-18 21:33:04 +0200

    terminar ya esta hecho

---
+++ src/partanalisis/ej2/entities/DVD.java
@@ -0,0 +1,13 @@
+package partanalisis.ej2.entities;
+
+public class DVD extends Ficha{
+
+	
+	@Override
+	int prestamo() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+
+}
--- src/partanalisis/ej2/entities/Ficha.java
+++ src/partanalisis/ej2/entities/Ficha.java
@@ -1,19 +1,66 @@
 package partanalisis.ej2.entities;
 
-public abstract class Ficha {
+public abstract class Ficha implements Comparable<Ficha>{
+	
+
+    private int numId;
+    private String titulo;
 	
 	
-	
-	
-	
+
+    public Ficha(int id, String titulo) {
+        
+    	if(id>=0) {
+    		this.numId = id;
+    	}
+    	
+        if(titulo!=null&&!titulo.equals("")) {
+        	this.titulo = titulo;
+        }
+    }
+
+    public int getId() {
+        return numId;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
 	
 	
 	/*
 	 * metodos abstractos
 	 */
 	
+	abstract int prestamo();
+	
+	/**
+	 * override de metodos
+	 */
 	
 	
+
+    @Override
+    public String toString() {
+    	String str="";
+    	str+="ID: "+numId+"\n";
+    	str+="Titulo: "+titulo+"\n";
+    	
+    	
+        return str;
+    }
+	
+	
+	@Override
+	public int compareTo(Ficha o) {
+		//llamamos al compare to del otro
+		int num=this.titulo.compareToIgnoreCase(o.titulo);
+				
+		return num;
+		
+	}
 	
 	
 }
+
